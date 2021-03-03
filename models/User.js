da1614b37672ce0206152cc6270f857d24415e7a
Shamim Imtiaz <km_si@ymail.com>
2021-03-03 13:26:51 -0500

    update password portion

--- models/User.js
+++ models/User.js
@@ -3,7 +3,12 @@ const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 //Create a user model
-class User extends Model {}
+class User extends Model {
+// set up method to run on instance data (per user) to check password
+checkPassword(loginPw) {
+  return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define table columns and configuration
 User.init(
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { route } = require('.');
 const { User } = require('../../models');
 
 // get all users
@@ -46,6 +47,30 @@ router.post('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+  
+      //res.json({ user: dbUserData });
+  
+      // Verify user
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if(!validPassword) {
+        res.status(400).json({message: 'Incorrect password'});
+        return;
+      }
+      res.json({user: dbUserData, message: 'You are logged in'});
+    });  
+  });
 
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
