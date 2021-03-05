1de892d6ca04caf1747ce8b688124eb70d387586
Shamim Imtiaz <km_si@ymail.com>
2021-03-05 15:17:24 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,18 @@ const Sequelize = require('sequelize');
 //import the .env file
 require('dotenv').config();
 // create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//to run the app remotely at Heroku and as well as locally
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3306 
+    port: 3306
   });
+}
 
 
 
